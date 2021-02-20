354154d4135ab49b22320aeee12fe5558e2fae7e
Boyi Ma <boyima98@gmail.com>
2021-02-20 14:48:04 -0500

    add required timer functions

--- Server/Singleton.js
+++ Server/Singleton.js
@@ -1,9 +1,23 @@
 
 // Some code need to be added here, that are common for the module
 
+let timer;
+
+function incrementTimer() {
+    // reset timer after reaching 2^32
+    if (timer === Math.pow(2, 32)) {
+        timer = 0;
+    }
+    timer++;
+}
+
 module.exports = {
     init: function() {
        // init function needs to be implemented here //
+        // initialize timer with a random number between 1 and 999
+        timer = Math.floor((Math.random() * 999) + 1);
+        // increment every 10ms
+        setInterval(incrementTimer, 10)
     },
 
     //--------------------------
@@ -18,7 +32,7 @@ module.exports = {
     //getTimestamp: return the current timer value
     //--------------------------
     getTimestamp: function() {
-        return "this should be a correct timestamp";
+        return timer;
     }
 
 
