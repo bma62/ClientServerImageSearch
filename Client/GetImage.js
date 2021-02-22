78eabb755fc2f2833c8223081d70631fed437b61
Boyi Ma <boyima98@gmail.com>
2021-02-21 23:04:41 -0500

    add minimal working version of client/server connection and communications

--- Client/GetImage.js
+++ Client/GetImage.js
@@ -2,6 +2,22 @@ let net = require("net");
 let fs = require("fs");
 let open = require("open");
 
-// let ITPpacket = require("./ITPRequest"); // uncomment this line after you run npm install command
+let ITPpacket = require("./ITPRequest"); // uncomment this line after you run npm install command
 
 // Enter your code for the client functionality here
+
+const client = new net.Socket();
+
+client.connect(3000, '127.0.0.1', () => {
+    console.log('Connected to the server.');
+    client.write('Hi server!')
+})
+
+client.on('data', (data) => {
+    console.log('Received: ' + data);
+    client.destroy(); // kill client after server's response
+});
+
+client.on('close', () => {
+    console.log('Connection closed');
+});
--- Server/ClientsHandler.js
+++ Server/ClientsHandler.js
@@ -2,7 +2,7 @@ let ITPpacket = require('./ITPResponse');
 let singleton = require('./Singleton');
 
 // You may need to add some delectation here
-
+let net = require('net')
 
 module.exports = {
 
@@ -12,6 +12,23 @@ module.exports = {
         //
         // you may need to develop some helper functions
         // that are defined outside this export block
+        let timeStamp = singleton.getTimestamp();
+        console.log(`\nClient-${timeStamp} is connected at timestamp: ${timeStamp}\n`);
+
+        // Receive data from the socket
+        sock.on('data', (packet) => {
+            // TODO: update to decode the packet
+            console.log(`Packet from client: ${packet}`);
+            sock.write('Hi client, the server got your msg.');
+        });
+
+        sock.on('end', () => {
+            console.log(`\nClient-${timeStamp} closed the connection.\n`);
+        });
+
+        sock.on('error', (err) => {
+            console.log(`Error: ${err}`);
+        });
     }
 };
 
