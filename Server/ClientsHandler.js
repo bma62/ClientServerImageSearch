8dad84b1d8b0c60aeb2b0b0d72c00a7b1dc08127
Boyi Ma <boyima98@gmail.com>
2021-03-01 17:58:13 -0500

    finish client packet decoding

--- Server/ClientsHandler.js
+++ Server/ClientsHandler.js
@@ -2,7 +2,8 @@ let ITPpacket = require('./ITPResponse');
 let singleton = require('./Singleton');
 
 // You may need to add some delectation here
-const net = require('net')
+const net = require('net');
+let version, imageCount , requestType, imageTypeArray = [], imageNameArray = [];
 
 module.exports = {
 
@@ -35,39 +36,49 @@ module.exports = {
 
 function decodePacket(packet, timeStamp) {
     console.log(`\nClient-${timeStamp} requests:`);
+
+    // First byte of packet
     let bufferOffset = 0;
+    let header = padStringToLength(int2bin(packet.readUInt8(bufferOffset)), 8);
 
-    let header = int2bin(packet.readUInt8(bufferOffset));
-    let version = bin2int(header.substring(0, 3));
+    // Bit 1-3 is version
+    version = bin2int(header.substring(0, 3));
     console.log(`\t--ITP version: ${version}`);
-    let imageCount = bin2int(header.substring(3));
-    console.log(`\t--Image count: ${imageCount}`);
-    bufferOffset = bufferOffset + 3;
 
-    header = int2bin(packet.readUInt8(bufferOffset));
-    let requestType = bin2int(header);
+    // Bit 4-8 is image count
+    imageCount = bin2int(header.substring(3));
+    console.log(`\t--Image count: ${imageCount}`);
+
+    bufferOffset = bufferOffset + 3; // Skip byte 2-3 as they are reserved and not used
+
+    // 4th byte is request type
+    header = padStringToLength(int2bin(packet.readUInt8(bufferOffset)), 8);
+    requestType = bin2int(header);
     if (requestType === 0) {
         console.log(`\t--Request type: Query`);
     }
+
+    // Repeat for the payload part to read image names and types
     ++bufferOffset;
-
-    let imageType = 0, imageTypeArray = [], imageName = '', imageNameArray = [], imageNameSize = 0;
+    let imageType, imageName = '', imageNameSize = 0;
     for (let i = 0; i < imageCount; i++) {
-        header = int2bin(packet.readUInt16BE(bufferOffset));
-        imageType = bin2int(header.substring(0, 4));
-        imageTypeArray.push(getImageExtension(imageType));
-        imageNameSize = bin2int(header.substring(4));
 
-        bufferOffset = bufferOffset + 2;
+        // First 2 bytes of payload is image type and image name size
+        header = padStringToLength(int2bin(packet.readUInt16BE(bufferOffset)), 16);
+        imageType = bin2int(header.substring(0, 4)); // Bit 1-4 is image type
+        imageTypeArray.push(getImageExtension(imageType)); // Convert to extension name and add to array
+        imageNameSize = bin2int(header.substring(4)); // Bit 5-16 is image name size
 
+        bufferOffset = bufferOffset + 2; // Shift buffer offset to read image name
+
+        // As this range of buffer is characters, they can be pushed together as the full name string
         imageNameArray.push(packet.slice(bufferOffset, bufferOffset + imageNameSize).toString());
 
-        bufferOffset = bufferOffset + imageNameSize;
+        bufferOffset = bufferOffset + imageNameSize; // Move on to next image
     }
 
     console.log(`\t--Image file extension(s): ${imageTypeArray.toString()}`);
     console.log(`\t--Image file name(s): ${imageNameArray.toString()}`);
-
 }
 
 function int2bin(int) {
@@ -76,6 +87,16 @@ function int2bin(int) {
 
 function bin2int(bin) {
     return parseInt(bin,2);
+}
+
+// pad binary strings to fixed length to avoid conversion issues between int and binary
+function padStringToLength(str, targetLength) {
+    if (str.length < targetLength) {
+        return str.padStart(targetLength, '0');
+    }
+    else if (str.length === targetLength) {
+        return str;
+    }
 }
 
 function getImageExtension(type) {
@@ -93,7 +114,6 @@ function getImageExtension(type) {
         case 15:
             return 'raw';
         default:
-            console.log(type);
-            throw new Error('Image type not supported!');
+            throw new Error(`Image type ${type} not supported!`);
     }
 }
